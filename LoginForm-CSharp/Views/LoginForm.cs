209e67a39b682de2dd379900dc24636ad5b0ec21
Anuja Nisal Yashohara <rmanuja2003@gmail.com>
2024-04-05 15:16:18 +0530

    Login backend done

--- LoginForm-CSharp/Views/LoginForm.cs
+++ LoginForm-CSharp/Views/LoginForm.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LoginForm_CSharp.Views;
+using MySqlConnector;
 
 namespace LoginForm_CSharp
 {
     public partial class LoginForm : Form
     {
+        MySqlConnection connection = new MySqlConnection("server=localhost;database=csharploginform;port=3306;username=root;password=");
+        MySqlCommand command;
+        MySqlDataReader dr;
         public LoginForm()
         {
             InitializeComponent();
@@ -19,7 +24,45 @@ namespace LoginForm_CSharp
 
         private void LoginButton_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (string.IsNullOrEmpty(LogUsernameText.Text) || string.IsNullOrEmpty(LogPasswordText.Text))
+                {
+                    MessageBox.Show("Please enter username and password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
 
+                connection.Open();
+                string selectQuery = "SELECT * FROM users WHERE Username = @Username AND Password = @Password";
+                command = new MySqlCommand(selectQuery, connection);
+                command.Parameters.AddWithValue("@Username", LogUsernameText.Text);
+                command.Parameters.AddWithValue("@Password", LogPasswordText.Text);
+                dr = command.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    MessageBox.Show("Login Successful", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Hide();
+                    DashboardForm dashboardForm = new DashboardForm();
+                    dashboardForm.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid username or password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }  
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+            
         }
 
         private void GoToRegisterButton_Click(object sender, EventArgs e)
